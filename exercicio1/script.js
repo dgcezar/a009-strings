7b36d8b1c1c97fd99d65c96b532943d9eaf9e78b
dgcezar <diego.cezar13@gmail.com>
2022-08-31 14:43:09 -0300

    Final - exercícios finalizados

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,5 @@
 const nomeDoUsuario = prompt("Insira o seu nome: ");
-const comidaUm = prompt(
-  "Agora preciso que fale qual a sua comida favorita: "
-);
+const comidaUm = prompt("Agora preciso que fale qual a sua comida favorita: ");
 const comidaDois = prompt("Insira a segunda comida favorita: ");
 const comidaTres = prompt("Insira a terceira comida favorita: ");
 
